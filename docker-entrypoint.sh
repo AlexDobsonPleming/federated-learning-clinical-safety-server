b46b3f815c79cbf95e0626c059e65ee040bc66f5
Alexander Dobson-Pleming <alexdobsonpleming@gmail.com>
2025-05-22 02:45:28 +0100

    moved docker entry to dedicated script

---
+++ docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+set -e
+
+echo "==> applying migrations"
+python manage.py migrate
+
+echo "==> seeding flmodels"
+python manage.py seed_flmodels
+
+echo "==> creating uploader"
+python manage.py create_uploader uploader1
+
+echo "==> starting server"
+exec python manage.py runserver 0.0.0.0:8000 --noreload
