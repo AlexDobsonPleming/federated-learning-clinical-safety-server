23d3453159c57cec654d687192abea6234a65cb6
Alexander Dobson-Pleming <alexdobsonpleming@gmail.com>
2025-05-22 15:13:43 +0100

    refactor of db initialisation

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env sh
 set -eux
 
+echo "==> wiping old database"
+/bin/rm -f /app/db.sqlite3
+
+echo "==> creating fresh sqlite3 file"
+/usr/bin/touch /app/db.sqlite3
+
+
 echo "==> applying migrations"
 python manage.py migrate
 
