a9c6e44b61e5d9aacdf785e8c792473616c7e0c8
Alexander Dobson-Pleming <alexdobsonpleming@gmail.com>
2025-05-22 16:01:52 +0100

    demo dockerfile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -17,4 +17,19 @@ python manage.py seed_flmodels
 echo "==> creating uploader"
 python manage.py create_uploader uploader1
 
+# if we’re in “demo” mode, create the demo superuser
+if [ -n "$DEMO_USERNAME" ] && [ -n "$DEMO_PASSWORD" ]; then
+  echo "==> ensuring demo user exists"
+  python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username="$DEMO_USERNAME").exists():
+    User.objects.create_superuser(
+        username="$DEMO_USERNAME",
+        email="${DEMO_EMAIL:-demo@example.com}",
+        password="$DEMO_PASSWORD"
+    )
+EOF
+fi
+
 exec "$@"
